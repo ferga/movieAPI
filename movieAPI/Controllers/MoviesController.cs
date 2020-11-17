faad33618cae0e9b33bda94289e50f2f3df5792c
georios fergadis <GFergadi@UKHAMCSERLYL1G>
2020-11-17 10:17:54 +0000

    Add project files.

---
+++ movieAPI/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using movieAPI.Entities;
+
+namespace movieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MoviesController> _logger;
+
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Movie{}).ToArray();
+        }
+    }
+}
---
+++ movieAPI/Entities/Movie.cs
@@ -0,0 +1,17 @@
+﻿ 
+namespace movieAPI.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    [JsonObject]
+    [Serializable]
+    public class Movie
+    {
+        public Movie()
+        {
+        }
+
+    }
+}
